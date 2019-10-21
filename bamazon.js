d1eba2857166c4d9edd29f1cc76b40f49c95cc50
Erwin Angeles <erwin@eang.media>
2019-10-21 14:29:12 -0500

    removed node_modules from repository

---
+++ bamazon.js
@@ -0,0 +1,95 @@
+const inquire = require('inquirer');
+const cTable = require('console.table');
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+});
+
+connection.query(
+    "SELECT * FROM products",
+    function(err, res) {
+      if (err) throw err;
+      console.table(res);
+      beginPurchase();
+});
+
+function beginPurchase() {
+    inquire.prompt(
+        [
+            {
+                type: "list",
+                name: "intro",
+                message: "What would you like to do?",
+                choices: ["Purchase an item", "Exit"]
+            }
+        ]
+    ).then(function(user) {
+        if(user.intro == "Purchase an item") {
+            inquire.prompt(
+                [
+                    {
+                      type: "input",
+                      name: "itemID",
+                      message: "Enter the item ID: "
+                    },
+                    {
+                      type: "input",
+                      name: "quantity",
+                      message: "Enter the quantity you would like to purchase: "
+                    }
+                ]
+            ).then(function(user) {
+                quantityCheck(user.itemID, user.quantity);
+            })
+        }
+    })
+}
+
+
+
+function quantityCheck(id, quantity) {
+    connection.query(`SELECT stock_quantity FROM products WHERE item_id=${id}`, function(err, res) {
+        if (err) throw err;
+        if ((res[0].stock_quantity > 0) && (quantity > res[0].stock_quantity)) {
+            connection.query(`UPDATE products SET stock_quantity=${res[0].stock_quantity}-${quantity} WHERE item_id=${id}`,
+                function(err) {
+                    if (err) throw err;
+                    console.log("\nPurchase successful!\n");
+                    totalCost(id, quantity);
+                })
+        }
+        else if (res[0].stock_quantity == 0 && quantity < res[0].stock_quantity) {
+            console.log("\nInsufficient quantity available!");
+        }
+    });
+}
+//"INSERT INTO products SET ?",{ flavor: "Rocky Road", price: 3.0, quantity: 50 },
+//"SELECT * FROM products" 
+
+function totalCost(id, quantity) {
+    connection.query(`SELECT price FROM products WHERE item_id=${id}`, function(err, res) {
+        if (err) throw err;
+        console.log(`Total cost: ${res[0].price * quantity}\n`);
+});
+}
+
+
+
+
+
+
